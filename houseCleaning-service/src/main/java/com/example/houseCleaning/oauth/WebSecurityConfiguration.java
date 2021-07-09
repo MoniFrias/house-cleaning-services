f2e25d2c3d7772f817a2af7cb4895dfa4232716d
Monica Frias <friasmoni18@gmail.com>
2021-07-09 13:42:04 -0500

    Add some changes and OAuth2

---
+++ houseCleaning-service/src/main/java/com/example/houseCleaning/oauth/WebSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.houseCleaning.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+	    return super.authenticationManagerBean();
+	}
+	
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user=User.builder().username("user").password(passwEncoder().encode("pass")).
+    			roles("USER").build();
+		UserDetails userAdmin=User.builder().username("admin").password(passwEncoder().encode("secret")).
+    			roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user,userAdmin);
+    }
+
+	@Bean
+    public PasswordEncoder passwEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+	
+
+}
